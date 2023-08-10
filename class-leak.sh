0c5c1fb8ae4881e2248a5dcd6764ea9fbaa1b053
Viktor Szépe <viktor@szepe.net>
2023-08-10 23:21:47 +0200

    Use wildcard in class-leak.sh

--- class-leak.sh
+++ class-leak.sh
@@ -3,26 +3,10 @@
 # php8.2 /usr/local/bin/composer config repositories.4 '{"type":"vcs","url":"https://github.com/szepeviktor/TomasBusinessVotruba_class-leak.git"}'
 # php8.2 /usr/local/bin/composer require --dev tomasvotruba/class-leak
 
-declare -a IGNORED_TYPES=(
-    # Hardcoded in the framework
-    'App\Http\Kernel'
-    'App\Exceptions\Handler'
-)
-
-declare -a SKIP_TYPES
-
-# Laravel event auto discovery
-while read -r LISTENER; do
-    IGNORED_TYPES+=( "App\\Listeners\\${LISTENER}" )
-done < <(find app/Listeners/ -type f -name "*.php" -print0 | xargs -0 -n 1 -- basename --suffix=.php)
-
-# Laravel policy auto discovery
-while read -r POLICY; do
-    IGNORED_TYPES+=( "App\\Policies\\${POLICY}" )
-done < <(find app/Policies/ -type f -name "*.php" -print0 | xargs -0 -n 1 -- basename --suffix=.php)
-
-for TYPE in "${IGNORED_TYPES[@]}"; do
-    SKIP_TYPES+=( "--skip-type" "${TYPE}" )
-done
-
-php8.2 vendor/bin/class-leak check app/ config/ routes/ "${SKIP_TYPES[@]}"
+# Skipped: Hardcoded in the Laravel Framework
+# Skipped: Laravel event auto discovery
+# Skipped: Laravel policy auto discovery
+php8.2 vendor/bin/class-leak check app/ config/ routes/ \
+    --skip-type 'App\Http\Kernel' --skip-type 'App\Exceptions\Handler' \
+    --skip-type 'App\Listeners\*' \
+    --skip-type 'App\Policies\*'
